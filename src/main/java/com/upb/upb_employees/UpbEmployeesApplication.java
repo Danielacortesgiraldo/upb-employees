0c9385d2cc8f80eca270c71f5c5630e3c0f9e28c
Danielacortesgiraldo <danielacortesgiraldo.1@gmail.com>
2025-05-07 19:23:53 -0500

    feat: add initial configuration

---
+++ src/main/java/com/upb/upb_employees/UpbEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.upb.upb_employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbEmployeesApplication.class, args);
+	}
+
+}
